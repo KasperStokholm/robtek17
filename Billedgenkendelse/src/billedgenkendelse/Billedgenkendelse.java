21373b918a1b72b97b6123f6bf54a8bffcb71005
HAKKETMAKKER <32928443+HAKKETMAKKER@users.noreply.github.com>
2017-10-19 13:12:59 +0200

    Ændring i fis

--- Billedgenkendelse/src/billedgenkendelse/Billedgenkendelse.java
+++ Billedgenkendelse/src/billedgenkendelse/Billedgenkendelse.java
@@ -15,7 +15,7 @@ public class Billedgenkendelse {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("sdsdsd");
+       
     }
     
 }
